5a0d61b54409c9bd5b915a522c3d868bbdfbcada
Sheli-Zisman <sheli.zisman1@gmail.com>
2025-04-09 14:47:27 +0300

    settings changes

--- MyFirstApp/main.js
+++ MyFirstApp/main.js
@@ -25,5 +25,17 @@
     btnMenu?.addEventListener("click", () => {
       popUpMenu?.classList.toggle("hidden");
     });
+    window.addEventListener("DOMContentLoaded", () => {
+      const greetingEl = document.getElementById("greeting");
+      const savedName = localStorage.getItem("userName");
+    
+      if (savedName && greetingEl) {
+        greetingEl.textContent = `! היי ${savedName}`;
+      }
+    });
+    
+
+
+    
 
   
